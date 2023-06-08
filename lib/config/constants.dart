7500a306c9e875ba7ef97bf6a36de8c92ab63bd9
Ravinodesousa <Ravinodesousa@gmail.com>
2023-06-09 01:20:11 +0530

    changed base url

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,3 @@
-final baseURL = "https://6d17-43-249-184-58.ngrok-free.app/";
-// final baseURL = "http://nosh.eu-4.evennode.com/";
+// final baseURL = "https://6d17-43-249-184-58.ngrok-free.app/";
+final baseURL = "http://nosh.eu-4.evennode.com/";
 final razorpayKey = "rzp_test_BvLgFLkSpGZw06";
