b9f43dec8fe6ab75c7529202ff10709e3201ecf2
Ravinodesousa <Ravinodesousa@gmail.com>
2023-06-21 20:01:50 +0530

    included feature to upload canteen cover image

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,3 @@
-final baseURL = "https://ddf7-43-249-184-193.ngrok-free.app/";
-// final baseURL = "http://nosh.eu-4.evennode.com/";
+// final baseURL = "https://ddf7-43-249-184-193.ngrok-free.app/";
+final baseURL = "http://nosh.eu-4.evennode.com/";
 final razorpayKey = "rzp_test_BvLgFLkSpGZw06";
